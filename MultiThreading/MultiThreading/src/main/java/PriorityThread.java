b32501650daa322b0a688e43e8b006eb09387e37
Avinash Takane <takaneavinash@gmail.com>
2024-11-12 20:02:28 +0530

    Priority Thread code

--- MultiThreading/MultiThreading/src/main/java/PriorityThread.java
+++ MultiThreading/MultiThreading/src/main/java/PriorityThread.java
@@ -1,14 +1,38 @@
-class P1 implements Runnable{
+class P1 implements Runnable {
     @Override
     public void run() {
-        for(int i=0; i<10; i++){
-            System.out.println(i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i+" ");
         }
+        System.out.println();
+    }
+}
+
+class P2 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 11; i <= 20; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 }
 
 public class PriorityThread {
     public static void main(String[] args) {
-        Thread t = new Thread(new P1());
+        Thread p1 = new Thread(new P1());
+        Thread p2 = new Thread(new P2());
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getPriority());
+
+        p1.setPriority(Thread.MAX_PRIORITY);
+        System.out.println(p1.getName());
+        System.out.println(p1.getPriority());
+        p2.setPriority(Thread.MIN_PRIORITY);
+        System.out.println(p2.getName());
+        System.out.println(p2.getPriority());
+
+        p2.start();
+        p1.start();
     }
 }
