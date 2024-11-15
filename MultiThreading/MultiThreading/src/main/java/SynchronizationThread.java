ab05006a94040f16a5b25ab56b9a7483d6484f5c
Avinash Takane <takaneavinash@gmail.com>
2024-11-15 19:44:34 +0530

    Synchronization example

---
+++ MultiThreading/MultiThreading/src/main/java/SynchronizationThread.java
@@ -0,0 +1,41 @@
+public class SynchronizationThread{
+    public static int counter=0;
+    public static synchronized void incrementCounter(){
+        counter++;
+    }
+
+    public static void process() {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    incrementCounter();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    incrementCounter();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try{
+            t1.join();
+            t2.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println(counter);
+    }
+    public static void main(String[] args) {
+        process();
+    }
+}
