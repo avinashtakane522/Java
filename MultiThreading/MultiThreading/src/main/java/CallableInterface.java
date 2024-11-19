006ed2de77eeb8075c6ee608fa4bf93b1ec06d3a
Avinash Takane <takaneavinash@gmail.com>
2024-11-19 19:03:30 +0530

    CallableInterface example

---
+++ MultiThreading/MultiThreading/src/main/java/CallableInterface.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class CallableInterface implements Callable<String> {
+    private int id;
+    public CallableInterface(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(2000);
+        return "Id : "+id;
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        List<Future<String>> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Future<String> e = executor.submit(new CallableInterface(i));
+            list.add(e);
+        }
+        for (Future<String> f : list) {
+            try {
+                System.out.println(f.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        executor.shutdown();
+    }
+}
