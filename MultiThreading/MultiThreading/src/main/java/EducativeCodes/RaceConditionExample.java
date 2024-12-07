0abd0364d5c78f3b4c80d15b18694a15db739fe2
Avinash Takane <takaneavinash@gmail.com>
2024-12-07 21:31:43 +0530

    Racecondition Example - Due to unused sync'n

---
+++ MultiThreading/MultiThreading/src/main/java/EducativeCodes/RaceConditionExample.java
@@ -0,0 +1,33 @@
+package EducativeCodes;
+
+class BankAccount {
+    private int balance = 100;
+
+    public void withdraw(int amount) {
+        if (balance >= amount) {
+            System.out.println(Thread.currentThread().getName() + " is withdrawing " + amount);
+            balance -= amount; // Deduct amount
+            System.out.println(Thread.currentThread().getName() + " completed withdrawal. Remaining balance: " + balance);
+        } else {
+            System.out.println(Thread.currentThread().getName() + " cannot withdraw. Insufficient balance!");
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
+
+public class RaceConditionExample {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+
+        // Two threads trying to withdraw money simultaneously
+        Thread t1 = new Thread(() -> account.withdraw(80), "Thread 1");
+        Thread t2 = new Thread(() -> account.withdraw(50), "Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
