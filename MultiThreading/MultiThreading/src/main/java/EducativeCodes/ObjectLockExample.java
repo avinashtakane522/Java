1d9e36a557c327c547aca4b797eb0d761cd4de34
Avinash Takane <takaneavinash@gmail.com>
2024-12-11 22:28:43 +0530

    ObjectLockExample Example

---
+++ MultiThreading/MultiThreading/src/main/java/EducativeCodes/ObjectLockExample.java
@@ -0,0 +1,27 @@
+package EducativeCodes;
+
+class ObjectLockClass {
+    public synchronized void method() {
+        System.out.println(Thread.currentThread().getName() + " executing instance method");
+    }
+}
+
+public class ObjectLock {
+    public static void main(String[] args) {
+        ObjectLockClass obj1 = new ObjectLockClass();
+        ObjectLockClass obj2 = new ObjectLockClass();
+
+        // Thread 1 working on obj1
+        Thread t1 = new Thread(() -> obj1.method());
+        // Thread 2 working on obj2
+        Thread t2 = new Thread(() -> obj2.method());
+
+        t1.start();
+        t2.start();
+    }
+}
+
+//Thread 1 locks obj1 and Thread 2 locks obj2.
+//Both threads are able to execute the synchronized method concurrently
+//because they are working with different instances (obj1 and obj2),
+//so each one locks its respective object.
